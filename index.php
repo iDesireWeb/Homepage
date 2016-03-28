ea30c1d21d49ff08a60ca94c96d2059eb87be7b3
TERRORGNOM <Jojo93@live.de>
2016-03-28 22:15:18 +0200

    settings.php zum Index hinzugefügt

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+
+require_once ("data/settings.php");
+
 require_once ("class/Core.php");
 require_once("class/UserSystem.php");
+
 if(id\Core::IsMaintained()) {
     echo "Wartungsarbeiten!!!";
     die();
