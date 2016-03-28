5a650384915e1eb551b0145cd8ba4a22f1dc5630
Tobias <eddi2345@live.de>
2016-03-28 21:59:02 +0200

    User.PhP

---
+++ class/Core.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tobias
+ * Date: 28.03.2016
+ * Time: 21:45
+ */
+
+namespace id;
+
+class Core
+{
+    public static function GetSiteName() {
+        return "TestSite";
+    }
+    public static function IsMaintained() {
+        return true;
+    }
+}
